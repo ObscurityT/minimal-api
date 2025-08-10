ebfcdf926ff81310b49609573253593119b01ed8
Amanda Nobre <84333023+ObscurityT@users.noreply.github.com>
2025-08-09 22:52:01 -0300

    Iniciando Projeto

---
+++ Dominio/DTO/LoginDTO.cs
@@ -0,0 +1,9 @@
+
+namespace minimalApi.DTOs;
+{
+    public class LoginDTO
+    {
+        public string Email { get; set; } = default!;
+        public string Senha { get; set; } = default!;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Identity.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (minimalApi.DTOs.LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    {
+        return Results.Ok("Login com sucesso");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
+
+app.Run();
+
+
